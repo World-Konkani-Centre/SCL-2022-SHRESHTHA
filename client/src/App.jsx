feca79c9ce25a2c9b231e6049a405ed9eb3c8781
Bhargavi Nayak <nayakbhargavi01@gmail.com>
2022-06-27 23:59:28 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+import { getTest } from "./functions/test";
+function App() {
+  const [data, setData] = useState("Hello World");
+  useEffect(() => {
+    getTest().then((res) => { 
+      setData(res.message);
+    }).catch((err) => console.log(err));
+    
+  }, [])
+  return (
+    <div className="App">
+      <h1>
+        {data}
+     </h1>
+    </div>
+  );
+}
+
+export default App;
